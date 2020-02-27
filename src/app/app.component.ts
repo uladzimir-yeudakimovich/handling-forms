182744cbddd6c43c69a6d62b0ba2884ac9328cb7
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-02-27 09:45:46 +0300

    Setting and Patching Form Values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,24 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    });
   }
 
   // onSubmit(form: NgForm) {
