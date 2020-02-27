cee54ad7e99cceb2eef040032e3796fecb8ef118
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-02-27 10:29:59 +0300

    started to learn reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
